9729ecc84b525bd5505aa3ae3c56635f2cdfef14
Purvika Jagtap <purvikajagtap@gmail.com>
2024-03-12 14:23:15 +0530

    Instagram clone main page

---
+++ src/pages/InstaMainPage.js
@@ -0,0 +1,355 @@
+const InstaMainPage = () => {
+  return (
+    <div className="w-full relative bg-white h-[838px] overflow-hidden text-center text-mini text-gray-200 font-sf-pro-text">
+      <div className="absolute h-[4.5%] top-[91.95%] bottom-[3.56%] left-[0px] bg-white box-border overflow-hidden flex flex-col items-start justify-start pt-1.5 px-[25px] pb-0.5 border-t-[0.5px] border-solid border-gray-500">
+        <div className="flex flex-row items-center justify-start gap-[0px_61px]">
+          <div className="flex flex-row items-start justify-start gap-[0px_57px]">
+            <img
+              className="w-[22px] relative h-[22.7px]"
+              alt=""
+              src="/shape.svg"
+            />
+            <img
+              className="w-[23.4px] relative h-[23.4px]"
+              alt=""
+              src="/shape.svg"
+            />
+            <img
+              className="w-[23.5px] relative h-[23.5px]"
+              alt=""
+              src="/shape.svg"
+            />
+          </div>
+          <div className="flex flex-row items-center justify-start gap-[0px_46px]">
+            <img
+              className="w-[22px] relative h-[22px] overflow-hidden shrink-0"
+              alt=""
+              src="/iconsreels.svg"
+            />
+            <img
+              className="w-7 relative rounded-[50%] h-7 object-cover"
+              alt=""
+              src="/ellipse-17@2x.png"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="absolute h-[5.25%] w-full top-[0%] right-[0%] bottom-[94.75%] left-[0%] overflow-hidden">
+        <img
+          className="absolute top-[calc(50%_-_4px)] right-[14.5px] w-[24.5px] h-[10.5px] object-cover"
+          alt=""
+          src="/battery@2x.png"
+        />
+        <img className="relative w-[15.9px] h-[11px]" alt="" src="/wifi.svg" />
+        <img
+          className="relative w-[17.7px] h-[10.7px]"
+          alt=""
+          src="/mobile-signal.svg"
+        />
+        <div className="absolute h-[40.91%] w-[14.41%] top-[31.82%] right-[80%] bottom-[27.27%] left-[5.59%] overflow-hidden">
+          <div className="absolute w-[calc(100%_-_0.2px)] top-[calc(50%_-_9px)] left-[0px] tracking-[-0.3px] font-semibold inline-block">
+            9:41
+          </div>
+        </div>
+      </div>
+      <div className="absolute h-[4.06%] w-full top-[95.94%] right-[0%] bottom-[0%] left-[0%] overflow-hidden">
+        <div className="absolute bottom-[9px] left-[calc(50%_-_67px)] rounded-81xl bg-gray-300 w-[134px] h-[5px]" />
+      </div>
+      <div className="absolute top-[46px] left-[calc(50%_-_195px)] w-[390px] h-[724px] overflow-y-auto text-xs text-black font-segoe-ui">
+        <img
+          className="absolute top-[20px] left-[150px] w-6 h-4 object-cover"
+          alt=""
+          src="/expand-arrow@2x.png"
+        />
+        <img
+          className="absolute top-[-14px] left-[-10px] w-[188px] h-[84px] object-cover"
+          alt=""
+          src="/insta-logo@2x.png"
+        />
+        <div className="absolute top-[14px] left-[301px] flex flex-row items-start justify-start gap-[0px_26px]">
+          <img
+            className="w-[22px] relative h-[18.3px]"
+            alt=""
+            src="/like.svg"
+          />
+          <img
+            className="w-[22px] relative h-[22px] overflow-hidden shrink-0"
+            alt=""
+            src="/iconsmessenger.svg"
+          />
+        </div>
+        <div className="absolute top-[166px] left-[10px] flex flex-row items-center justify-start">
+          <img
+            className="w-10 relative rounded-[50%] h-[41px] object-cover z-[0]"
+            alt=""
+            src="/ellipse-6@2x.png"
+          />
+          <div className="w-28 relative tracking-[-0.3px] flex items-center justify-center h-9 shrink-0 z-[1] ml-[-7px]">
+            mumbaiindians
+          </div>
+          <img
+            className="w-[13px] absolute !m-[0] top-[14px] left-[132px] h-[13px] object-cover z-[2]"
+            alt=""
+            src="/62a56496137de6cc96c044fcfd732908removebgpreview-1@2x.png"
+          />
+        </div>
+        <img
+          className="absolute top-[213px] left-[0px] w-[390px] h-[258px] object-cover"
+          alt=""
+          src="/whatsapp-image-20240309-at-720-2@2x.png"
+        />
+        <img
+          className="absolute top-[176px] left-[360px] w-[22px] h-[22px] overflow-hidden object-contain"
+          alt=""
+          src="/iconsmore@2x.png"
+        />
+        <div className="absolute top-[481px] left-[17px] flex flex-row items-start justify-start gap-[0px_223px]">
+          <div className="flex flex-row items-end justify-start gap-[0px_21px]">
+            <img
+              className="w-[22px] relative h-[19.1px]"
+              alt=""
+              src="/like.svg"
+            />
+            <img
+              className="w-[22px] relative h-[22px] overflow-hidden shrink-0"
+              alt=""
+              src="/iconscomment.svg"
+            />
+            <img
+              className="w-[22px] relative h-[22px] overflow-hidden shrink-0"
+              alt=""
+              src="/share.svg"
+            />
+          </div>
+          <img
+            className="w-[22px] relative h-[22px] overflow-hidden shrink-0"
+            alt=""
+            src="/iconssave.svg"
+          />
+        </div>
+        <div className="absolute top-[503px] left-[17px] w-[388px] h-16">
+          <div className="absolute top-[0px] left-[0px] flex flex-col items-start justify-start">
+            <div className="w-[59px] relative tracking-[-0.3px] flex items-center justify-center h-[26px] shrink-0">
+              35,394 likes
+            </div>
+            <div className="w-[388px] relative tracking-[-0.3px] text-left flex items-center h-[22px] shrink-0 mt-[-10px]">
+              <span className="w-full">
+                <span>
+                  <span className="whitespace-pre-wrap">{`mumbaiindians  One step closer from the `}</span>
+                  <b className="font-segoe-ui">{`Q `}</b>
+                  <span className="text-darkgray-100">stamp</span>
+                </span>
+                <span className="text-darkgray-100">
+                  <span className="text-darkgray-100"> .</span>
+                  <span>...more</span>
+                </span>
+              </span>
+            </div>
+          </div>
+          <div className="absolute top-[34px] left-[0px] w-[166px] h-[30px] text-left text-darkgray-100">
+            <div className="absolute top-[0px] left-[0px] tracking-[-0.3px] flex items-center w-[165px] h-[18px]">
+              view all 55 comments
+            </div>
+            <div className="absolute top-[21px] left-[0px] flex flex-row items-center justify-start gap-[0px_7px]">
+              <img
+                className="w-[17px] relative rounded-[50%] h-[17px] object-cover"
+                alt=""
+                src="/ellipse-9@2x.png"
+              />
+              <div className="w-[82px] relative tracking-[-0.3px] flex items-center h-2.5 shrink-0">
+                Add a comment
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="absolute top-[575px] left-[17px] text-[10px] tracking-[-0.3px] text-darkgray-100 text-left flex items-center w-[166px] h-[18px]">
+          6 hours ago
+        </div>
+        <div className="absolute top-[609px] left-[0px] w-[390px] h-[255px] overflow-x-auto text-left">
+          <div className="absolute top-[3px] left-[0px] bg-gray-100 w-[548px] h-[252px]" />
+          <div className="absolute top-[5px] left-[9px] tracking-[-0.3px] flex items-center w-[178px] h-7">
+            Suggested for you
+          </div>
+          <div className="absolute top-[5px] left-[345px] tracking-[-0.3px] text-cornflowerblue flex items-center w-[45px] h-7">
+            See all
+          </div>
+          <div className="absolute top-[45px] left-[373px] flex flex-row items-start justify-start gap-[0px_10px]">
+            <div className="w-[161px] rounded-10xs bg-white box-border h-[197px] overflow-hidden shrink-0 flex flex-row items-start justify-end py-1.5 px-2 z-[0] border-[0.2px] border-solid border-darkgray-300">
+              <img
+                className="w-2.5 relative h-[11px]"
+                alt=""
+                src="/vector.svg"
+              />
+            </div>
+            <img
+              className="w-[99px] absolute !m-[0] top-[12px] left-[28px] rounded-[50%] h-[100px] object-cover z-[1]"
+              alt=""
+              src="/ellipse-12@2x.png"
+            />
+            <div className="!m-[0] absolute top-[114px] left-[34px] flex flex-col items-center justify-start z-[2]">
+              <div className="w-[46px] relative tracking-[-0.3px] flex items-center h-6 shrink-0">
+                vraj birje
+              </div>
+              <div className="w-[93px] relative text-2xs tracking-[-0.3px] flex items-center h-5 shrink-0 mt-[-7px] text-darkgray-100">
+                <span className="w-full">
+                  <span>suggested for you</span>
+                  <span>{` `}</span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="absolute top-[45px] left-[17px] flex flex-row items-start justify-start gap-[0px_10px]">
+            <div className="w-[161px] rounded-10xs bg-white box-border h-[197px] overflow-hidden shrink-0 flex flex-row items-start justify-end py-1.5 px-2 z-[0] border-[0.2px] border-solid border-darkgray-300">
+              <img
+                className="w-2.5 relative h-[11px]"
+                alt=""
+                src="/vector.svg"
+              />
+            </div>
+            <img
+              className="w-[99px] absolute !m-[0] top-[12px] left-[28px] rounded-[50%] h-[100px] object-cover z-[1]"
+              alt=""
+              src="/ellipse-10@2x.png"
+            />
+            <div className="!m-[0] absolute top-[114px] left-[34px] flex flex-col items-center justify-start z-[2]">
+              <div className="w-20 relative tracking-[-0.3px] flex items-center h-6 shrink-0">
+                Ojas Tamhankar
+              </div>
+              <div className="w-[93px] relative text-2xs tracking-[-0.3px] flex items-center h-5 shrink-0 mt-[-7px] text-darkgray-100">
+                <span className="w-full">
+                  <span>suggested for you</span>
+                  <span>{` `}</span>
+                </span>
+              </div>
+            </div>
+            <div className="w-[136px] !m-[0] absolute top-[160px] left-[12px] rounded-8xs bg-cornflowerblue h-6 overflow-hidden shrink-0 flex flex-row items-center justify-center z-[3] text-center text-2xs text-white">
+              <div className="relative tracking-[-0.3px]">Follow</div>
+            </div>
+          </div>
+          <div className="absolute top-[45px] left-[195px] flex flex-row items-start justify-start gap-[0px_10px]">
+            <div className="w-[161px] rounded-10xs bg-white box-border h-[197px] overflow-hidden shrink-0 flex flex-row items-start justify-end py-1.5 px-2 z-[0] border-[0.2px] border-solid border-darkgray-300">
+              <img
+                className="w-2.5 relative h-[11px]"
+                alt=""
+                src="/vector.svg"
+              />
+            </div>
+            <img
+              className="w-[99px] absolute !m-[0] top-[17px] left-[28px] h-[100px] object-cover z-[1]"
+              alt=""
+              src="/ellipse-11@2x.png"
+            />
+            <div className="!m-[0] absolute top-[114px] left-[34px] flex flex-col items-center justify-start z-[2]">
+              <div className="w-20 relative tracking-[-0.3px] flex items-center h-6 shrink-0">
+                Rachit chheda
+              </div>
+              <div className="w-[93px] relative text-2xs tracking-[-0.3px] flex items-center h-5 shrink-0 mt-[-7px] text-darkgray-100">
+                <span className="w-full">
+                  <span>suggested for you</span>
+                  <span>{` `}</span>
+                </span>
+              </div>
+            </div>
+            <div className="w-[136px] !m-[0] absolute top-[160px] left-[12px] rounded-8xs bg-cornflowerblue h-6 overflow-hidden shrink-0 flex flex-row items-center justify-center z-[3] text-center text-2xs text-white">
+              <div className="relative tracking-[-0.3px]">Follow</div>
+            </div>
+          </div>
+          <div className="absolute top-[205px] left-[385px] rounded-8xs bg-cornflowerblue w-[136px] h-6 overflow-hidden flex flex-row items-center justify-center text-center text-2xs text-white">
+            <div className="relative tracking-[-0.3px]">Follow</div>
+          </div>
+        </div>
+        <img
+          className="absolute top-[150px] left-[0px] w-[390px] h-px object-contain"
+          alt=""
+        />
+        <div className="absolute top-[51px] left-[-10px] w-[400px] h-[94px] overflow-x-auto">
+          <img
+            className="absolute top-[7px] left-[22px] w-[66px] h-16 object-contain"
+            alt=""
+            src="/frame-20@2x.png"
+          />
+          <div className="absolute top-[3px] left-[108px] flex flex-col items-start justify-start gap-[10px_0px]">
+            <img
+              className="w-[74px] relative h-[72px] z-[0]"
+              alt=""
+              src="/subtract.svg"
+            />
+            <img
+              className="w-[66px] absolute !m-[0] top-[4px] left-[4px] rounded-[50%] h-16 object-cover z-[1]"
+              alt=""
+              src="/ellipse-6@2x.png"
+            />
+          </div>
+          <div className="absolute top-[3px] left-[191px] flex flex-col items-start justify-start gap-[10px_0px]">
+            <img
+              className="w-[74px] relative h-[72px] z-[0]"
+              alt=""
+              src="/subtract.svg"
+            />
+            <img
+              className="w-[66px] absolute !m-[0] top-[4px] left-[4px] rounded-[50%] h-16 object-cover z-[1]"
+              alt=""
+              src="/ellipse-6@2x.png"
+            />
+          </div>
+          <div className="absolute top-[3px] left-[274px] flex flex-col items-start justify-start gap-[10px_0px]">
+            <img
+              className="w-[74px] relative h-[72px] z-[0]"
+              alt=""
+              src="/subtract.svg"
+            />
+            <img
+              className="w-[66px] absolute !m-[0] top-[4px] left-[4px] rounded-[50%] h-16 object-cover z-[1]"
+              alt=""
+              src="/ellipse-6@2x.png"
+            />
+          </div>
+          <div className="absolute top-[3px] left-[357px] flex flex-col items-start justify-start gap-[10px_0px]">
+            <img
+              className="w-[74px] relative h-[72px] z-[0]"
+              alt=""
+              src="/subtract.svg"
+            />
+            <img
+              className="w-[66px] absolute !m-[0] top-[4px] left-[4px] rounded-[50%] h-16 object-cover z-[1]"
+              alt=""
+              src="/ellipse-6@2x.png"
+            />
+          </div>
+          <div className="absolute top-[3px] left-[440px] flex flex-col items-start justify-start gap-[10px_0px]">
+            <img
+              className="w-[74px] relative h-[72px] z-[0]"
+              alt=""
+              src="/subtract.svg"
+            />
+            <img
+              className="w-[66px] absolute !m-[0] top-[4px] left-[4px] rounded-[50%] h-16 object-cover z-[1]"
+              alt=""
+              src="/ellipse-6@2x.png"
+            />
+          </div>
+          <div className="absolute top-[77px] left-[17px] tracking-[-0.3px] inline-block w-[81px] h-3.5">
+            Your story
+          </div>
+          <div className="absolute top-[77px] left-[118px] tracking-[-0.3px] inline-block w-[60px] h-3.5">
+            user_1
+          </div>
+          <div className="absolute top-[77px] left-[201px] tracking-[-0.3px] inline-block w-[60px] h-3.5">
+            user_2
+          </div>
+          <div className="absolute top-[77px] left-[284px] tracking-[-0.3px] inline-block w-[60px] h-3.5">
+            user_3
+          </div>
+          <div className="absolute top-[77px] left-[367px] tracking-[-0.3px] inline-block w-[60px] h-3.5">
+            user_4
+          </div>
+          <div className="absolute top-[77px] left-[450px] tracking-[-0.3px] inline-block w-[60px] h-3.5">
+            user_5
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default InstaMainPage;
